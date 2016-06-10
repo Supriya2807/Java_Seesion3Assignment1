b703b3c212ce6153d612e3e9142c0d47bffb9812
Supriya2807 <lahadesupriya@gmail.com>
2016-06-10 17:32:29 +0530

    session3assign1
    sqrt and cbrt

---
+++ session3assign1.java
@@ -0,0 +1,16 @@
+package acadgild.assignments;
+
+public class session3assign1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// To find sqrt and cbrt of a no. using method and Math class
+		
+		int num=25;
+		System.out.println("Squareroot of "+num+" is :"+(Math.sqrt(num)));
+		System.out.println("Cuberoot of "+num+" is :"+(Math.cbrt(num)));
+		
+
+	}
+
+}
